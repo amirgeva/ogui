060eacc5f7bf1a7bb354bb4b4e295b2ca6d5e286
Amir Geva <amirgeva@gmail.com>
2014-01-11 18:28:32 +0200

    Fixed file headers

--- src/ogui/def_font.h
+++ src/ogui/def_font.h
@@ -1,5 +1,5 @@
 /***************************************************************************
-Copyright (c) 2013, Amir Geva
+Copyright (c) 2013-2014, Amir Geva
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
