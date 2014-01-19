3b4c3dd90cf9803f2b92844221607033d0883dc5
Amir Geva <amirgeva@gmail.com>
2014-01-19 11:05:23 +0200

    Removed duplicate utility function

--- include/ogui/math2d.h
+++ include/ogui/math2d.h
@@ -31,8 +31,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 namespace OGUI {
 
-template<class T> inline T sqr(T t)      { return t*t;  }
-
 template<class T>
 struct TPoint
 {
